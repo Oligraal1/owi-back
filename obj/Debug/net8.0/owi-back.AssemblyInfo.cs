46f60be49603420b78c0745391ba388269aeb2ad
Webira <guery.ira@gmail.com>
2024-06-25 13:28:12 +0200

    fixe merge

--- obj/Debug/net8.0/owi-back.AssemblyInfo.cs
+++ obj/Debug/net8.0/owi-back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("owi-back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a1c266326b593e69aa1c08aa2244c6947367e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba114e76348455c2874466fbe62708df5973d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("owi-back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("owi-back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
