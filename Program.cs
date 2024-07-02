c12781ef7eb3e2117311dca764128388c8402c38
HollyWeb <46522066+Oligraal1@users.noreply.github.com>
2024-07-02 13:33:57 +0200

    version pour deploiement"

--- Program.cs
+++ Program.cs
@@ -1,33 +1,7 @@
-using owi_back.Context;
-using owi_back.DAO;
-using owi_back.Mapping;
-using owi_back.Models;
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<OwidbContext>();
-builder.Services.AddScoped<TaskDAO>();
-builder.Services.AddScoped<CommentDAO>();
-builder.Services.AddScoped<Mapper>();
-builder.Services.AddScoped<ListingDao>();
-builder.Services.AddScoped<ProjectDao>();
-
-// Configure CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("http://localhost:4200")
-                          .AllowAnyMethod()
-                          .AllowAnyHeader());
-});
-
-builder.Services.AddSwaggerGen(c =>
-{
-   c.SwaggerDoc("v1", new OpenApiInfo { Title = "Owi Api", Version = "v1" });
-});
 
 var app = builder.Build();
 
@@ -39,20 +13,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-if (builder.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Owi Api v1"));
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-// Use the CORS policy
-app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
